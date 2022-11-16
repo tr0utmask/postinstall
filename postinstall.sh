f897cb875b13300d446cd9891ec480150e9c148f
tr0utmask <114903732+tr0utmask@users.noreply.github.com>
2022-11-16 18:29:53 +0200

    Clone repo

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,9 @@
 
 # Arch Linux post-installation script
 
+## Go home
+cd
+
 ## Update system
 sudo pacman -Syu
 
@@ -17,4 +20,9 @@ cd
 ## When yay is installed, install the necessary packages
 yay -S adobe-source-han-sans-otc-fonts dunst feh firefox gnome-themes-extra gtk-engines gvim htop libnotify lxappearance mpv mupdf pulseaudio pulseaudio-alsa pulsemixer rxvt-unicode scrot thunar ttf-go-sans-git ttf-ms-fonts tumbler unrar unzip xorg-fonts-misc xorg-server xorg-xinit xorg-xrandr xorg-xset xorg-xsetroot yt-dlp
 
-echo "Please install the appropriate graphics drivers yourself."
+printf "\nPlease install the appropriate graphics drivers yourself.\n"
+
+## Clone dotfiles repo
+git clone https://github.com/tr0utmask/dotfiles
+
+## Build suckless utilities
