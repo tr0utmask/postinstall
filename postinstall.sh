ff5fc8570e4f4dcab049e4054c47d9a7620c4363
tr0utmask <114903732+tr0utmask@users.noreply.github.com>
2022-11-16 23:57:22 +0200

    Add bash-completion to list of packages

--- postinstall.sh
+++ postinstall.sh
@@ -18,7 +18,7 @@ makepkg --noconfirm -si
 cd ~ || exit
 
 ## When yay is installed, install the necessary packages
-yay -S adobe-source-han-sans-otc-fonts dunst feh firefox gnome-themes-extra gtk-engines gvim htop libnotify lxappearance mpv mupdf pulseaudio pulseaudio-alsa pulsemixer rxvt-unicode scrot thunar ttf-go-sans-git ttf-ms-fonts tumbler unrar unzip xcape xorg-fonts-misc xorg-server xorg-xinit xorg-xrandr xorg-xset xorg-xsetroot yt-dlp
+yay -S adobe-source-han-sans-otc-fonts bash-completion dunst feh firefox gnome-themes-extra gtk-engines gvim htop libnotify lxappearance mpv mupdf pulseaudio pulseaudio-alsa pulsemixer rxvt-unicode scrot thunar ttf-go-sans-git ttf-ms-fonts tumbler unrar unzip xcape xorg-fonts-misc xorg-server xorg-xinit xorg-xrandr xorg-xset xorg-xsetroot yt-dlp
 
 ## Clone dotfiles repo
 git clone https://github.com/tr0utmask/dotfiles
