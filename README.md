1b4f1810760898b75c39e2b7e6817a1db64ffcf3
tr0utmask <114903732+tr0utmask@users.noreply.github.com>
2023-01-08 13:31:19 +0200

    Simplify script

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Post-installation script for Arch Linux and Debian
+# Post-installation script for Arch Linux
 
-A simple script that will install my personal dotfiles on a base install of Arch Linux or Debian. The script assumes that you don't have pre-existing configurations, as those will be mercilessly overwritten. Run the script as a regular user that you intend to use the system as. Make sure you have sudo privileges.
+A simple script that will install my personal dotfiles on a base install of Arch Linux. The script assumes that you don't have pre-existing configurations, as those will be mercilessly overwritten. Run the script as a regular user that you intend to use the system as. Make sure you have sudo privileges.
--- postinstall.sh
+++ postinstall.sh
@@ -1,65 +1,24 @@
 #!/bin/sh
 
-# Post-installation script for Arch Linux and Debian
+# Post-installation script for Arch Linux
 
 ## Go home
 cd ~ || exit
 
-## Determine which OS we're running
-if [ "$(pacman -V)" ]; then
+## Update system
+sudo pacman --noconfirm -Syu
 
-	# Arch-specific tasks
+## Install git
+sudo pacman --noconfirm -S git
 
-	## Update system
-	sudo pacman --noconfirm -Syu
+## Install yay
+git clone https://aur.archlinux.org/yay
+cd yay || exit
+makepkg --noconfirm -si
+cd ~ || exit
 
-	## Install git
-	sudo pacman --noconfirm -S git
-
-	## Install yay
-	git clone https://aur.archlinux.org/yay
-	cd yay || exit
-	makepkg --noconfirm -si
-	cd ~ || exit
-
-	## When yay is installed, install the necessary packages
-	yay -S adobe-source-han-sans-otc-fonts bash-completion dunst feh firefox gnome-themes-extra gtk-engines gvim htop libnotify lxappearance mpv mupdf pulseaudio pulseaudio-alsa pulsemixer rxvt-unicode scrot thunar ttf-go-sans-git ttf-ms-fonts tumbler unrar unzip xcape xorg-fonts-misc xorg-server xorg-xinit xorg-xrandr xorg-xset xorg-xsetroot yt-dlp
-
-elif [ "$(apt -v)" ]; then
-
-	# Debian-specific tasks
-
-	operating_system=debian
-
-	## Update system
-	sudo apt update && sudo apt upgrade
-
-	## Install necessary packages
-	sudo apt install --no-install-recommends bash-completion build-essential curl dunst feh firefox-esr fonts-go git gnome-themes-extra gtk2-engines htop libx11-dev libxft-dev libxinerama-dev libxrandr-dev lm-sensors lxappearance mpv mupdf pulseaudio pulsemixer rxvt-unicode scrot thunar unrar-free unzip vim-gtk3 x11-xserver-utils xcape xfonts-base xinit xserver-xorg-core
-
-	## Install yt-dlp
-	sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
-	sudo chmod a+rx /usr/local/bin/yt-dlp
-
-	## Temporarily enable contrib repos and install MS fonts
-	echo "deb http://ftp.de.debian.org/debian bullseye main contrib" | sudo tee -a /etc/apt/sources.list
-	sudo apt update
-	sudo apt install --no-install-recommends ttf-mscorefonts-installer
-	sudo sed -i '$ d' /etc/apt/sources.list
-
-	## Allow use of bitmap fonts
-	cd /etc/fonts/conf.d/
-	sudo rm /etc/fonts/conf.d/10*
-	sudo rm -rf 70-no-bitmaps.conf
-	sudo ln -s ../conf.avail/70-yes-bitmaps.conf .
-	sudo dpkg-reconfigure fontconfig
-	cd ~ || exit
-
-else
-
-	echo "You seem to be using an unsupported operating system. Only Arch Linux and Debian are officially supported. Their derivatives may also work, but support for them is not guaranteed." && exit
-
-fi
+## When yay is installed, install the necessary packages
+yay -S adobe-source-han-sans-otc-fonts bash-completion dunst feh firefox gnome-themes-extra gtk-engines gvim htop libnotify lxappearance mpv mupdf pulseaudio pulseaudio-alsa pulsemixer rxvt-unicode scrot thunar ttf-go-sans-git ttf-go-mono-git ttf-ms-fonts tumbler unrar unzip xcape xorg-fonts-misc xorg-server xorg-xinit xorg-xrandr xorg-xset xorg-xsetroot yt-dlp
 
 ## Clone dotfiles repo
 git clone https://github.com/tr0utmask/dotfiles
@@ -80,20 +39,11 @@ cp -rv dotfiles/.config/* ~/.config
 
 ## Build suckless utilities
 cd ~/.local/src/dwm || exit
-if [ "$operating_system" = "debian" ]; then
-	sed -i 's/Misc\ //g' ~/.local/src/dwm/config.h
-fi
 sudo make clean install
 cd ~/.local/src/dmenu || exit
-if [ "$operating_system" = "debian" ]; then
-	sed -i 's/Misc\ //g' ~/.local/src/dmenu/config.h
-fi
 sudo make clean install
 cd ~/.local/src/slock || exit
 sed -i "s/\"user\"/\"$(whoami)\"/" config.h
-if [ "$operating_system" = "debian" ]; then
-	sed -i 's/wheel/sudo/' config.h
-fi
 sudo make clean install
 
 ## Disable pc speaker
@@ -102,5 +52,4 @@ echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
 
 ## Finalize
 rm -v ~/.bash_*
-clear
-printf "The installation finished succesfully. Install the appropriate graphics drivers and reboot the machine to get started.\n\n"
+printf "\nThe installation finished succesfully. Install the appropriate graphics drivers and reboot the machine to get started.\n\n"
